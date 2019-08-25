2895d469bceaf8566ac35e92b2f23fe0167cfba7
Pierpaolo <pierpaolo.pdd@gmail.com>
2019-08-25 12:23:39 +0200

    Charge request object

---
+++ src/main/java/com/soa/stripe/dto/ChargeRequest.java
@@ -0,0 +1,24 @@
+package com.soa.stripe.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChargeRequest {
+ 
+    public enum Currency {
+        EUR, USD;
+    }
+    private String description;
+    private int amount;
+    private Currency currency;
+    private String stripeEmail;
+    private String stripeToken;
+}
