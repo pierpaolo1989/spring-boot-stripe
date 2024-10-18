4de8b23e7023c96dab115b85293bb839de28fa47
pierpaolo1989 <pierpaolo.pdd@gmail.com>
2024-10-18 18:42:28 +0200

    Update properties file and renamed controllers

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM eclipse-temurin:17-jdk-alpine
+WORKDIR /opt/app
+COPY .mvn/ .mvn
+COPY mvnw pom.xml ./
+RUN ./mvnw dependency:go-offline
+COPY ./src ./src
+RUN ./mvnw clean install -DskipTests
+
+FROM eclipse-temurin:17-jdk-alpine
+WORKDIR /opt/app
+EXPOSE 8080
+COPY --from=builder /opt/app/target/*.jar /opt/app/*.jar
+ENTRYPOINT ["java","-Dspring.profiles.active=prod", "-jar", "/opt/app/*.jar" ]
--- src/main/java/com/soa/stripe/controller/ChargeRestController.java
+++ src/main/java/com/soa/stripe/controller/ChargeRestController.java
@@ -5,8 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,33 +13,18 @@ import org.springframework.web.bind.annotation.RestController;
 import com.soa.stripe.dto.ChargeRequest;
 import com.soa.stripe.service.IStripeService;
 import com.stripe.model.Charge;
-import com.stripe.model.Customer;
 
 @RestController
-@RequestMapping("/stripe")
-public class StripeRestController {
+@RequestMapping("/charge")
+public class ChargeRestController {
 
-	private static final Logger logger = LoggerFactory.getLogger(StripeRestController.class.getName());
-	
+	private static final Logger logger = LoggerFactory.getLogger(ChargeRestController.class.getName());
+
 	@Autowired
 	private IStripeService stripeService;
-	
-	@GetMapping(value="/{id}")
-	public ResponseEntity<Customer> getCustomer(@PathVariable("id") String id) throws Exception{
-		HttpStatus status = null;
-		Customer customer = null;
-		try {
-			customer = stripeService.getCustomer(id);
-			status = HttpStatus.OK;
-		} catch (Exception e) {
-			logger.error("Impossible to retrieve the customer with ID: {}",id,e.getMessage());
-			throw(e);
-		}
-		return new ResponseEntity<Customer>(customer,status);
-	}
-	
+
 	@PostMapping(value="/charge")
-	public ResponseEntity<Charge> charge(@RequestBody ChargeRequest request) throws Exception{
+	public ResponseEntity<Charge> charge(@RequestBody ChargeRequest request) throws Exception {
 		HttpStatus status = null;
 		Charge charge = null;
 		try {
@@ -51,6 +34,7 @@ public class StripeRestController {
 			logger.error("Impossible to execute the charge",e.getMessage());
 			throw(e);
 		}
-		return new ResponseEntity<Charge>(charge,status);
+		return new ResponseEntity<Charge>(charge, status);
 	}
 }
+
